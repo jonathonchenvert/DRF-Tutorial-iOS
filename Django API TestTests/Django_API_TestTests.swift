a527e551e8fa2d055e7d8daba7144e0609b523e4
Jonathon Chenvert <jchenvert1@gmail.com>
2020-12-19 15:37:25 -0800

    Initial Commit

---
+++ Django API TestTests/Django_API_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Django_API_TestTests.swift
+//  Django API TestTests
+//
+//  Created by Jonathon Chenvert on 12/19/20.
+//
+
+import XCTest
+@testable import Django_API_Test
+
+class Django_API_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
