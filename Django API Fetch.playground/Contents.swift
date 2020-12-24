847664a4356bfb7391c752a1ff69297b039a6c1a
Jonathon Chenvert <jchenvert1@gmail.com>
2020-12-23 20:46:46 -0800

    DRF Tutorial 3 complete; put CRUD functionality into individual functions for easier execution one-by-one

--- Django API Fetch.playground/Contents.swift
+++ Django API Fetch.playground/Contents.swift
@@ -4,143 +4,173 @@ import SwiftyJSON
 
 // MARK: - Data retrieval for all snippets from Django API setup
 
-let getSnippetsURL = "http://127.0.0.1:8000/snippets/"
+func getAllSnippets(url: String) {
+    
+    AF.request(url, encoding: JSONEncoding.default).responseJSON { response in
 
-AF.request(getSnippetsURL, encoding: JSONEncoding.default).responseJSON { response in
+        switch response.result {
+        case .success:
+            print("Validation successful!")
+            let dataJSON: JSON = JSON(response.value!)
+            //print("Data: \(dataJSON)")
+            //print("Count: \(dataJSON.count)")
 
-    switch response.result {
-    case .success:
-        print("Validation successful!")
-        let dataJSON: JSON = JSON(response.value!)
-        //print("Data: \(dataJSON)")
-        //print("Count: \(dataJSON.count)")
+            for data in 0...dataJSON.count {
+                guard let title = dataJSON[data]["title"].string else { return }
+                guard let code = dataJSON[data]["code"].string else { return }
+                guard let linenos = dataJSON[data]["linenos"].bool else { return }
+                guard let language = dataJSON[data]["language"].string else { return }
+                guard let style = dataJSON[data]["style"].string else { return }
 
-        for data in 0...dataJSON.count {
-            guard let title = dataJSON[data]["title"].string else { return }
-            guard let code = dataJSON[data]["code"].string else { return }
-            guard let linenos = dataJSON[data]["linenos"].bool else { return }
-            guard let language = dataJSON[data]["language"].string else { return }
-            guard let style = dataJSON[data]["style"].string else { return }
+                print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
+            }
 
-            print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
+        case let .failure(error):
+            print("Error accessing network: \(error)")
         }
-
-    case let .failure(error):
-        print("Error accessing network: \(error)")
-    }
-}
-
-// MARK: - Data retrieval for a specific snippet
-
-let firstSnippetURL = "http://127.0.0.1:8000/snippets/1/"
-
-var headers: HTTPHeaders
-
-headers = [
-    .accept("application/json")
-]
-
-AF.request(firstSnippetURL, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-
-    switch response.result {
-    case .success:
-        print("Validation successful!")
-
-        let dataJSON: JSON = JSON(response.value!)
-        //print("Data: \(dataJSON)")
-
-        guard let title = dataJSON["title"].string else { return }
-        guard let code = dataJSON["code"].string else { return }
-        guard let linenos = dataJSON["linenos"].bool else { return }
-        guard let language = dataJSON["language"].string else { return }
-        guard let style = dataJSON["style"].string else { return }
-
-        print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
-
-    case let .failure(error):
-        print("Error accessing network: \(error)")
-    }
-}
-
-// MARK: - Data update for a specific snippet
-
-let code: String = "print('Hello, world!')"
-let title: String = "Hello World in Python"
-
-let parameters = [
-    "code": code,
-    "title": title
-]
-
-AF.request(firstSnippetURL, method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-
-    switch response.result {
-    case .success:
-        print("Validation successful!")
-
-        let dataJSON: JSON = JSON(response.value!)
-        //print("Data: \(dataJSON)")
-
-        guard let title = dataJSON["title"].string else { return }
-        guard let code = dataJSON["code"].string else { return }
-        guard let linenos = dataJSON["linenos"].bool else { return }
-        guard let language = dataJSON["language"].string else { return }
-        guard let style = dataJSON["style"].string else { return }
-
-        print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
-
-    case let .failure(error):
-        print("Error accessing network: \(error)")
-    }
-}
-
-// MARK: - Data deletion for a specific snippet
-
-let fourthSnippetURL = "http://127.0.0.1:8000/snippets/4/"
-
-AF.request(fourthSnippetURL, method: .delete, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-
-    switch response.result {
-    case .success:
-        print("Validation successful!")
-        print("Data has been deleted")
-
-    case let .failure(error):
-        print("Error accessing network: \(error)")
     }
 }
 
 // MARK: - Data creation for a new snippet
 
-let newCode: String = "int myNum = 15;"
-let newTitle: String = "Assign Variable in Java"
-let newLanguage: String = "java"
+func createSnippet(url: String, headers: HTTPHeaders, parameters: [String:String]) {
+    
+    AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
+        
+        switch response.result {
+        case .success:
+            print("Validation successful!")
 
-let newParameters = [
+            let dataJSON: JSON = JSON(response.value!)
+            //print("Data: \(dataJSON)")
+
+            guard let title = dataJSON["title"].string else { return }
+            guard let code = dataJSON["code"].string else { return }
+            guard let linenos = dataJSON["linenos"].bool else { return }
+            guard let language = dataJSON["language"].string else { return }
+            guard let style = dataJSON["style"].string else { return }
+
+            print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
+
+        case let .failure(error):
+            print("Error accessing network: \(error)")
+        }
+
+    }
+}
+
+// MARK: - Data retrieval for a specific snippet
+
+func retrieveSnippet(url: String, headers: HTTPHeaders) {
+    
+    AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+
+        switch response.result {
+        case .success:
+            print("Validation successful!")
+
+            let dataJSON: JSON = JSON(response.value!)
+            //print("Data: \(dataJSON)")
+
+            guard let title = dataJSON["title"].string else { return }
+            guard let code = dataJSON["code"].string else { return }
+            guard let linenos = dataJSON["linenos"].bool else { return }
+            guard let language = dataJSON["language"].string else { return }
+            guard let style = dataJSON["style"].string else { return }
+
+            print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
+
+        case let .failure(error):
+            print("Error accessing network: \(error)")
+        }
+    }
+}
+
+// MARK: - Data update for a specific snippet
+
+func updateSnippet(url: String, headers: HTTPHeaders, parameters: [String:String]) {
+    
+    AF.request(url, method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
+
+        switch response.result {
+        case .success:
+            print("Validation successful!")
+
+            let dataJSON: JSON = JSON(response.value!)
+            //print("Data: \(dataJSON)")
+
+            guard let title = dataJSON["title"].string else { return }
+            guard let code = dataJSON["code"].string else { return }
+            guard let linenos = dataJSON["linenos"].bool else { return }
+            guard let language = dataJSON["language"].string else { return }
+            guard let style = dataJSON["style"].string else { return }
+
+            print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
+
+        case let .failure(error):
+            print("Error accessing network: \(error)")
+        }
+    }
+}
+
+// MARK: - Data deletion for a specific snippet
+
+func deleteSnippet(url: String, headers: HTTPHeaders) {
+    
+    AF.request(url, method: .delete, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
+
+        switch response.result {
+        case .success:
+            print("Validation successful!")
+            print("Data has been deleted")
+
+        case let .failure(error):
+            print("Error accessing network: \(error)")
+        }
+    }
+}
+
+// MARK: - Variables for passing to CRUD functions
+
+// URL to create snippet / get all snippets
+let getSnippetsURL = "http://127.0.0.1:8000/snippets/"
+
+// URL to get first snippet
+let firstSnippetURL = "http://127.0.0.1:8000/snippets/1/"
+
+var httpHeaders: HTTPHeaders
+
+httpHeaders = [
+    .accept("application/json")
+]
+
+// Parameters to update first snippet
+let code: String = "print('I am a print statement.')"
+let title: String = "Generic print statement in Python"
+
+let passedParameters = [
+    "code": code,
+    "title": title
+]
+
+// URL to delete a specific snippet
+let deleteSnippetURL = "http://127.0.0.1:8000/snippets/7/"
+
+// Parameters to create a snippet
+let newCode: String = "var five = 5"
+let newTitle: String = "Assign Mutable Variable in Swift"
+let newLanguage: String = "swift"
+
+let createParameters = [
     "code": newCode,
     "title": newTitle,
     "language": newLanguage
 ]
 
-AF.request(getSnippetsURL, method: .post, parameters: newParameters, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-    
-    switch response.result {
-    case .success:
-        print("Validation successful!")
+// MARK: - Calling of CRUD functions
 
-        let dataJSON: JSON = JSON(response.value!)
-        //print("Data: \(dataJSON)")
-
-        guard let title = dataJSON["title"].string else { return }
-        guard let code = dataJSON["code"].string else { return }
-        guard let linenos = dataJSON["linenos"].bool else { return }
-        guard let language = dataJSON["language"].string else { return }
-        guard let style = dataJSON["style"].string else { return }
-
-        print("Title: \(title)\nLanguage: \(language)\nCode: \(code)Linenos? \(linenos)\nStyle: \(style)\n")
-
-    case let .failure(error):
-        print("Error accessing network: \(error)")
-    }
-
-}
+//getAllSnippets(url: getSnippetsURL)
+//createSnippet(url: getSnippetsURL, headers: httpHeaders, parameters: createParameters)
+//retrieveSnippet(url: firstSnippetURL, headers: httpHeaders)
+//updateSnippet(url: firstSnippetURL, headers: httpHeaders, parameters: passedParameters)
+//deleteSnippet(url: deleteSnippetURL, headers: httpHeaders)
